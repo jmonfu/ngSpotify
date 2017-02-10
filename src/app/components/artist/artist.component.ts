78e58d2243bb02b5bf7a429f114edab4114e7c91
Johann Montfort <jmonfu@gmail.com>
2017-02-10 17:05:38 +0000

    getting the albums for the artist

--- src/app/components/artist/artist.component.ts
+++ src/app/components/artist/artist.component.ts
@@ -28,6 +28,12 @@ export class ArtistComponent implements OnInit {
             .subscribe(artist => {
               this.artist = artist;
             })
+
+          this._spotifyService.getAlbums(id)
+            .subscribe(albums => {
+              this.albums = albums.items;
+            })
+
         })
   }
 
